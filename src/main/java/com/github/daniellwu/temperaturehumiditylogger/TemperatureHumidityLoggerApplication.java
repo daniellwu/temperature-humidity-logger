c9c73b04a25748823683099547850a9f6bd95575
Daniel Wu <daniellwu@users.noreply.github.com>
2018-05-29 10:09:52 -0700

    add spring boot, gradle skeleton frameworks

---
+++ src/main/java/com/github/daniellwu/temperaturehumiditylogger/TemperatureHumidityLoggerApplication.java
@@ -0,0 +1,12 @@
+package com.github.daniellwu.temperaturehumiditylogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureHumidityLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureHumidityLoggerApplication.class, args);
+	}
+}
